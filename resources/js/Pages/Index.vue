bb0bf5e57afb0d1c89c3c9e59360f924e6b6e879
andrea <andrea.zollini@proton.me>
2024-02-10 15:11:35 +0100

    initial html setup

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,9 +1,22 @@
 <template>
-
+    <div class="flex flex-col items-center mt-10">
+        <h1 class="text-3xl">Inertia Task Manager</h1>
+        <div class="mt-5 flex flex-col items-center">
+            <input type="text" v-model="newTask" placeholder="New Task..." class="p-2 rounded shadow"
+                style="text-indent: 10px;">
+            <button class="rounded-full bg-blue-400 py-3 px-5 mt-4 max-w-fit" @click="addTask()">Add Task</button>
+        </div>
+    </div>
 </template>
 
 <script setup>
+    import { ref } from 'vue';
 
+    const newTask = ref(null);
+
+    const addTask = () => {
+        console.log(newTask.value);
+    }
 </script>
 
 <style lang="scss" scoped>
